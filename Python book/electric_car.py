536f3800f107898d9a14cb06587409a5ecd77c11
oboyle3 <po720999@sju.edu>
2024-07-29 21:21:33 -0400

    new car

--- Python book/electric_car.py
+++ Python book/electric_car.py
@@ -1,17 +1,5 @@
 # electric_car.py
 from car import Car
-#
-# class Car:
-#     def __init__(self, make, model, year):
-#         # Initialize attributes to describe a car
-#         self.make = make
-#         self.model = model
-#         self.year = year
-
-#     def get_descriptive_name(self):
-#         # Return a neatly formatted name
-#         long_name = f"{self.make} {self.model} {self.year}"
-#         return long_name.title()
 
 
 
@@ -26,5 +14,8 @@ class ElectricCar(Car):
         print(f"The battery size is {self.battery_size} kWh")
 
 my_leaf = ElectricCar('Nissan', 'Leaf Hybrid x2', 2024)
+my_car = ElectricCar('chevy','bolt', 2021)
 print(my_leaf.get_descriptive_name())
 my_leaf.describe_battery()
+
+
